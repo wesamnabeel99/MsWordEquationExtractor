b8edd103a158d57afedf1f384a11cc22f76b6885
Wesam Nabeel <wesamezio@gmail.com>
2023-02-04 20:04:29 +0300

    improve code

--- MsWordEquationExtractor.cs
+++ MsWordEquationExtractor.cs
@@ -13,32 +13,31 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            Aspose.Words.Document doc = new Aspose.Words.Document(@"C:\Users\Wesam Nabeel\x1.docx");
+            string inputFile = @"C:\Users\Wesam Nabeel\x1.docx";
+            string outputFile = "C:\\Users\\Wesam Nabeel\\equations.pdf";
+            int extractedEquations = ExtractEquationsToPDF(inputFile, outputFile);
+
+            Console.WriteLine(extractedEquations + " equations extracted");
+            Console.WriteLine("Press Any Key To Continue...");
+            Console.ReadKey();
+
+        }
+
+        private static int ExtractEquationsToPDF(string inputFile, string outputFile)
+        {
+            Aspose.Words.Document doc = new Aspose.Words.Document(inputFile);
 
             int equationNumber = 0;
 
             Document pdfDoc = new Document();
-            PdfWriter writer = PdfWriter.GetInstance(pdfDoc, new System.IO.FileStream("C:\\Users\\Wesam Nabeel\\equations.pdf", System.IO.FileMode.Create));
+            PdfWriter writer = PdfWriter.GetInstance(pdfDoc, new System.IO.FileStream(outputFile, System.IO.FileMode.Create));
             pdfDoc.Open();
 
             foreach (Aspose.Words.Paragraph pargraph in doc.GetChildNodes(NodeType.Paragraph, true))
             {
-                NodeCollection equations = pargraph.GetChildNodes(NodeType.OfficeMath, false);
-                foreach (OfficeMath equation in equations)
+                foreach (OfficeMath equation in pargraph.GetChildNodes(NodeType.OfficeMath, false))
                 {
-                    Bitmap image = new Bitmap(1, 1);
-                    Graphics graphics = Graphics.FromImage(image);
-
-                    SizeF size = equation.GetMathRenderer().RenderToScale(graphics, 0f, 0f, 2f);
-
-                    int width = (int)Math.Ceiling(size.Width);
-                    int height = (int)Math.Ceiling(size.Height);
-
-                    image = new Bitmap(width, height);
-                    graphics = Graphics.FromImage(image);
-
-                    equation.GetMathRenderer().RenderToScale(graphics, 0f, 0f, 2f);
-
+                    Bitmap image = GetEquationImage(equation);
                     Console.WriteLine("equation " + (equationNumber + 1) + ": " + equation.GetText());
 
                     Image iTextSharpImage = Image.GetInstance(image, System.Drawing.Imaging.ImageFormat.Png);
@@ -52,8 +51,34 @@ namespace Example
             }
 
             pdfDoc.Close();
-            Console.WriteLine(equationNumber + " equations extracted");
-            Console.ReadKey();
+            return equationNumber;
+        }
+
+        private static Bitmap GetEquationImage(OfficeMath equation)
+        {
+            SizeF size = GetImageSize(equation);
+
+            int width = (int)Math.Ceiling(size.Width);
+            int height = (int)Math.Ceiling(size.Height);
+
+            Bitmap image = new Bitmap(width, height);
+
+            Graphics graphics = Graphics.FromImage(image);
+
+            equation.GetMathRenderer().RenderToScale(graphics, 0f, 0f, 2f);
+
+            return image;
+        }
+
+        private static SizeF GetImageSize(OfficeMath equation)
+        {
+            Bitmap placeHolderImage = new Bitmap(1, 1);
+            Graphics placeHolderGraphics = Graphics.FromImage(placeHolderImage);
+
+            return equation.GetMathRenderer().RenderToScale(placeHolderGraphics, 0f, 0f, 2f);
+
         }
     }
+
+
 }
