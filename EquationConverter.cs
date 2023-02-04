3e1dbfe5ab1998d1d0bf771ee5d5a5881687a88f
Wesam Nabeel <wesamezio@gmail.com>
2023-02-04 20:24:14 +0300

    create new class to improve code

---
+++ EquationConverter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using Aspose.Words;
+using Aspose.Words.Math;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Document = iTextSharp.text.Document;
+using Image = iTextSharp.text.Image;
+using Rectangle = iTextSharp.text.Rectangle;
+
+namespace EquationToImageConverter
+{
+    public class EquationConverter
+    {
+        private readonly string _inputFilePath;
+        private readonly string _outputFilePath;
+        private int _equationNumber;
+
+        public EquationConverter(string inputFilePath, string outputFilePath)
+        {
+            _inputFilePath = inputFilePath;
+            _outputFilePath = outputFilePath;
+        }
+
+        public int Convert()
+        {
+            Aspose.Words.Document doc = new Aspose.Words.Document(_inputFilePath);
+            Document pdfDoc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(pdfDoc, new System.IO.FileStream(_outputFilePath, System.IO.FileMode.Create));
+            pdfDoc.Open();
+
+            foreach (Aspose.Words.Paragraph paragraph in doc.GetChildNodes(NodeType.Paragraph, true))
+            {
+                NodeCollection equations = paragraph.GetChildNodes(NodeType.OfficeMath, false);
+                foreach (OfficeMath equation in equations)
+                {
+                    Bitmap image = GetEquationImage(equation);
+
+                    Console.WriteLine("equation " + (++_equationNumber) + ": " + equation.GetText());
+
+                    Image iTextSharpImage = Image.GetInstance(image, System.Drawing.Imaging.ImageFormat.Png);
+                    WriteToNewPdfPage(pdfDoc,iTextSharpImage);
+                }
+            }
+
+            pdfDoc.Close();
+            return _equationNumber;
+        }
+
+
+
+        private Bitmap GetEquationImage(OfficeMath equation)
+        {
+            SizeF size = GetImageSize(equation);
+
+            int width = (int)Math.Ceiling(size.Width);
+            int height = (int)Math.Ceiling(size.Height);
+
+            Bitmap image = new Bitmap(width, height);
+
+            Graphics graphics = Graphics.FromImage(image);
+
+            equation.GetMathRenderer().RenderToScale(graphics, 0f, 0f, 2f);
+
+            return image;
+        }
+
+        private Size GetImageSize(OfficeMath equation)
+        {
+            Bitmap placeHolderImage = new Bitmap(1, 1);
+            Graphics placeHolderGraphics = Graphics.FromImage(placeHolderImage);
+            SizeF size = equation.GetMathRenderer().RenderToScale(placeHolderGraphics, 0f, 0f, 2f);
+            int width = (int)Math.Ceiling(size.Width);
+            int height = (int)Math.Ceiling(size.Height);
+            return new Size(width, height);
+        }
+
+        private void WriteToNewPdfPage(Document pdfDoc, Image iTextSharpImage)
+        {
+            pdfDoc.SetPageSize(new Rectangle(0, 0, iTextSharpImage.Width, iTextSharpImage.Height));
+            pdfDoc.NewPage();
+            iTextSharpImage.SetAbsolutePosition(0, 0);
+            pdfDoc.Add(iTextSharpImage);
+        }
+    }
+}
--- MsWordEquationExtractor.cs
+++ MsWordEquationExtractor.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Drawing;
-using Aspose.Words;
-using Aspose.Words.Math;
-using iTextSharp.text.pdf;
-using Document = iTextSharp.text.Document;
-using Image = iTextSharp.text.Image;
-using Rectangle = iTextSharp.text.Rectangle;
+
+using EquationToImageConverter;
 
 namespace Example
 {
@@ -15,7 +10,8 @@ namespace Example
         {
             string inputFile = @"C:\Users\Wesam Nabeel\x1.docx";
             string outputFile = "C:\\Users\\Wesam Nabeel\\equations.pdf";
-            int extractedEquations = ExtractEquationsToPDF(inputFile, outputFile);
+            EquationConverter equationToImage = new EquationConverter(inputFile, outputFile);
+            int extractedEquations = equationToImage.Convert();
 
             Console.WriteLine(extractedEquations + " equations extracted");
             Console.WriteLine("Press Any Key To Continue...");
@@ -23,61 +19,8 @@ namespace Example
 
         }
 
-        private static int ExtractEquationsToPDF(string inputFile, string outputFile)
-        {
-            Aspose.Words.Document doc = new Aspose.Words.Document(inputFile);
+    
 
-            int equationNumber = 0;
-
-            Document pdfDoc = new Document();
-            PdfWriter writer = PdfWriter.GetInstance(pdfDoc, new System.IO.FileStream(outputFile, System.IO.FileMode.Create));
-            pdfDoc.Open();
-
-            foreach (Aspose.Words.Paragraph pargraph in doc.GetChildNodes(NodeType.Paragraph, true))
-            {
-                foreach (OfficeMath equation in pargraph.GetChildNodes(NodeType.OfficeMath, false))
-                {
-                    Bitmap image = GetEquationImage(equation);
-                    Console.WriteLine("equation " + (equationNumber + 1) + ": " + equation.GetText());
-
-                    Image iTextSharpImage = Image.GetInstance(image, System.Drawing.Imaging.ImageFormat.Png);
-                    pdfDoc.SetPageSize(new Rectangle(0, 0, iTextSharpImage.Width, iTextSharpImage.Height));
-                    pdfDoc.NewPage();
-                    iTextSharpImage.SetAbsolutePosition(0, 0);
-                    pdfDoc.Add(iTextSharpImage);
-
-                    equationNumber++;
-                }
-            }
-
-            pdfDoc.Close();
-            return equationNumber;
-        }
-
-        private static Bitmap GetEquationImage(OfficeMath equation)
-        {
-            SizeF size = GetImageSize(equation);
-
-            int width = (int)Math.Ceiling(size.Width);
-            int height = (int)Math.Ceiling(size.Height);
-
-            Bitmap image = new Bitmap(width, height);
-
-            Graphics graphics = Graphics.FromImage(image);
-
-            equation.GetMathRenderer().RenderToScale(graphics, 0f, 0f, 2f);
-
-            return image;
-        }
-
-        private static SizeF GetImageSize(OfficeMath equation)
-        {
-            Bitmap placeHolderImage = new Bitmap(1, 1);
-            Graphics placeHolderGraphics = Graphics.FromImage(placeHolderImage);
-
-            return equation.GetMathRenderer().RenderToScale(placeHolderGraphics, 0f, 0f, 2f);
-
-        }
     }
 
 
